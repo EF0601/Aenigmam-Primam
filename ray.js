4fd30bcd0e1cada88ee2d799cbd32d1dda336271
EF0601 <edwardfang0601@gmail.com>
2023-04-04 12:52:09 -0400

    updated keyhole

--- ray.js
+++ ray.js
@@ -1,3 +1,8 @@
+/*
+A WARNING TO THOSE WHO ARE NOT THE DEVELOPERS OF THE PROJECT AND IS A COMPETITOR OF THE EVENT
+IF YOU ARE CAUGHT LOOKING FOR INFORMATION IN THE CODE, YOU WILL NOT GET THE REWARD.
+FURTHERMORE, WE CAN SEE WHO HAS DOWNLOADED THE GITHUB PROJECT ANYWAY.
+*/
 //inputs
 let valReplace = "";
 let passReplace = "";
@@ -14,7 +19,7 @@ function verify() {
     document.querySelector(".verification").style.display = "none";
   }
   else {
-    location.reload()
+    location.reload();
   }
 }
 //puzzle functions
@@ -26,11 +31,15 @@ function passInput() {
 
 function turnsTheDoor() {
   if (valReplace === "R T") {
-    alert("You have intelligence.");
+    keyhole.textContent = "You have intelligence. You have solved it.";
+    keyhole.style.color = "blue";
   }
   else {
-    alert("You lack specialty. You shall restart.");
-    location.reload()
+    keyhole.textContent= "You lack specialty. You shall restart.";
+    keyhole.style.color = "red";
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
   }
 }
     //anti-brute force functions
