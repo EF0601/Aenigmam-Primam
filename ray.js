29bc4dfd1bbb25aab546a4b3450f98c3cd466f5b
EF0601 <edwardfang0601@gmail.com>
2023-04-04 13:30:19 -0400

    failed cookie features

--- ray.js
+++ ray.js
@@ -35,11 +35,40 @@ function turnsTheDoor() {
     keyhole.style.color = "blue";
   }
   else {
-    keyhole.textContent= "You lack specialty. You shall restart.";
+    keyhole.textContent = "You lack specialty. You shall restart.";
     keyhole.style.color = "red";
     setTimeout(() => {
       location.reload();
-    }, 1000);
+    }, 2500);
   }
 }
-    //anti-brute force functions
+//anti-brute force functions (fail)
+
+
+// const d = new Date();
+// let minutes = d.getMinutes();
+
+// function cooldown() {
+//   if (minutes != 59) {
+//     document.cookie = "nextTime=" + minutes + "; expires=" + new Date(new Date().getTime() + 180 * 1000).toUTCString() + "; path=/";
+//     console.log("Cookie set with value: " + minutes);
+//     console.log(String(document.cookie.replace(/(?:(?:^|.*;\s*)nextTime\s*\=\s*([^;]*).*$)|^.*$/, "$1")));
+//   }
+// }
+
+
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(';');
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) == ' ') {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) == 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
