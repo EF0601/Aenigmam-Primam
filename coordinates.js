78833713099fcc7d12795e39ebb846cbb5178aa9
EF0601 <edwardfang0601@gmail.com>
2023-08-25 11:11:02 +0800

    Coordinates.html

---
+++ coordinates.js
@@ -0,0 +1,31 @@
+/*
+<!-- A WARNING TO SEEKERS, SHOULD YOU BE CAUGHT LOOKING FOR INFORMATION WITHIN THE CODE, YOUR ENTRY IS TERMINATED.
+WE CAN SEE WHO HAS DOWNLOADED THE GITHUB PROJECT -->
+*/
+//inputs
+let valReplace = "";
+
+//locations
+const keyhole = document.querySelector('.keyhole');
+//verify functions
+function inputVal() {
+  const val = document.querySelector('.input').value;
+  console.log(val);
+  valReplace = val;
+}
+//puzzle functions
+
+
+function turnsTheDoor() {
+  if (valReplace === "1 Medical Center Dr, Lebanon, NH 03766, United States") {
+    document.querySelector('#reply').style.display = "block";
+    document.querySelector('.input').disabled = true;
+  }
+  else {
+    document.querySelector('#wrong').style.display = "block";
+    document.querySelector('.input').disabled = true;
+    setTimeout(() => {
+      location.reload();
+    }, 2500);
+  }
+}
